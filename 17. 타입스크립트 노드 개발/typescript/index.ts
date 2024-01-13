97b196da4b06d5e746a56ccd762e22ea4404429e
unknown <lerle144@gmail.com>
2024-01-14 07:02:07 +0900

    #32 17.1 타입스크립트 기본 문법

---
+++ 17. 타입스크립트 노드 개발/typescript/index.ts
@@ -0,0 +1,5 @@
+let a: boolean = true;
+const b: { hello: string } = { hello: 'world' };
+
+function add(x: number, y: number): number { return x + y };
+const minus = (x: number, y: number): number => x - y;
